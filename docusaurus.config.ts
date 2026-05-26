1a0191fd6fce988f2a832731022eae4d8a01d2ef
Tao Dong <taojdcn@gmail.com>
2026-05-26 15:41:44 -0700

    prepare for release

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://waterwheel.duotail.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'taodong', // Usually your GitHub org/user name.
+  projectName: 'waterwheel-agent-site', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
